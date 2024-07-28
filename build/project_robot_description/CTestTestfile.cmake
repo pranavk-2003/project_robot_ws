917da3838f245da4c7e336eede214182245d9fa4
Pranav <pranavkolekar13@gmail.com>
2024-07-28 12:08:58 +0530

    first commit

---
+++ build/project_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pranav/project_robot_ws/src/project_robot_description
+# Build directory: /home/pranav/project_robot_ws/build/project_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
